58f49f2e33e0280d55fbfe28a2abfde7d6f98402
May360-ai <maya2017.hotmail@gamil>
2025-11-03 23:00:41 -0500

    Login Simulado

--- FacturasSRI.Web/Program.cs
+++ FacturasSRI.Web/Program.cs
@@ -1,8 +1,9 @@
 using FacturasSRI.Web.Components;
+using FacturasSRI.Web.States;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddSingleton<CurrentUserState>(); 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
@@ -10,11 +11,9 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
